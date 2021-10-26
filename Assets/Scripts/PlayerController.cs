27f5741d0d307320984cf9c8aa7d00e15f181f23
Victorpetts <vike.pet@hotmail.com>
2021-10-26 22:11:31 +0200

    Player turns

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,22 @@ public class PlayerController : MonoBehaviour {
     
     public float speed;
     private Animator animator;
+    private SpriteRenderer sprite;
 
     private void Start() {
         animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     private void Update() {
         Vector2 dir = Vector2.zero;
         if (Input.GetKey(KeyCode.A)) {
             dir.x = -1;
+            sprite.flipX = false;
         }
         else if (Input.GetKey(KeyCode.D)) {
             dir.x = 1;
+            sprite.flipX = true;
         }
 
         if (Input.GetKey(KeyCode.W)) {
