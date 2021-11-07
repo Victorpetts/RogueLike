3b8dd05551c3bd7f1c5977183dd457ddfdc63691
Victorpetts <vike.pet@hotmail.com>
2021-11-07 16:58:26 +0100

    Pretties up that waypoint function

--- Assets/Scripts/EnemyBat.cs
+++ Assets/Scripts/EnemyBat.cs
@@ -12,15 +12,9 @@ public class EnemyBat : EnemyController {
         base.Chase();
         
         if (Vector2.Distance(transform.position, target.position) > aggroRange) {
-            // if (Vector2.Distance(transform.position, waypointTarget.position) < 0.01f) {
-            //     waypointTarget = wayPoint1 ? wayPoint2 : wayPoint1;
-            // }            
             if (Vector2.Distance(transform.position, waypointTarget.position) < 0.01f) {
-                waypointTarget = wayPoint2;
-            }
-            if (Vector2.Distance(transform.position, wayPoint2.position) < 0.01f) {
-                waypointTarget = wayPoint1;
-            }
+                waypointTarget = waypointTarget == wayPoint1 ? wayPoint2 : wayPoint1;
+            } 
             
             transform.position =
                 Vector2.MoveTowards(transform.position, waypointTarget.position, moveSpeed * Time.deltaTime);
