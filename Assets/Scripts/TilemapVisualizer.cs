49a5d90d23bca953b838bf878fd7fe386dae629a
Victorpetts <vike.pet@hotmail.com>
2021-12-06 02:07:34 +0100

    Exit it on a separate tilemap

--- Assets/Scripts/TilemapVisualizer.cs
+++ Assets/Scripts/TilemapVisualizer.cs
@@ -5,20 +5,23 @@ using UnityEngine.Tilemaps;
 
 public class TilemapVisualizer : MonoBehaviour {
     [SerializeField] private Tilemap floorTilemap;
+    [SerializeField] private Tilemap exitTilemap;
     [SerializeField] private TileBase floorTile;
     [SerializeField] private TileBase exitTile;
 
     public void PaintFloorTiles(IEnumerable<Vector2Int> floorPositions) {
         PaintTiles(floorPositions, floorTilemap, floorTile);
+        PaintExit(floorPositions, exitTilemap, exitTile);
     }
 
     private void PaintTiles(IEnumerable<Vector2Int> positions, Tilemap tilemap, TileBase tile) {
         foreach (var position in positions) {
             PaintSingleTile(tilemap, tile, position);
         }
-
+    }
+    private void PaintExit(IEnumerable<Vector2Int> positions, Tilemap tilemap, TileBase tile) {
         var exitPos = positions.ElementAt(Random.Range(0, positions.Count()));
-        PaintSingleTile(tilemap, exitTile, exitPos);
+        PaintSingleTile(tilemap, tile, exitPos);
     }
 
     private void PaintSingleTile(Tilemap tilemap, TileBase tile, Vector2Int position) {
@@ -28,5 +31,6 @@ public class TilemapVisualizer : MonoBehaviour {
 
     public void Clear() {
         floorTilemap.ClearAllTiles();
+        exitTilemap.ClearAllTiles();
     }
 }
